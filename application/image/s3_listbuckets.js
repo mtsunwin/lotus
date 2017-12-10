b33ea9bbfe378d4afecd75e50629faf1e552548f
Tran Thang <mt.tranthang@gmail.com>
2017-12-10 12:52:26 +0700

    S3 AWS update

--- application/image/s3_listbuckets.js
+++ application/image/s3_listbuckets.js
@@ -1,20 +1,3 @@
-// Load the SDK for JavaScript
-// var AWS = require('aws-sdk');
-// Set the region
-// AWS.config.update({region: 'ap-southeast-1'});
-// AWS.config.accessKeyId = "AKIAIPWOWY7BRDRICJ3Q";
-// AWS.config.secretAccessKey = "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";
-// Create S3 service object
-// s3 = new AWS.S3({apiVersion: '2006-03-01'});
-// // Call S3 to list current buckets
-// s3.listBuckets(function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         console.log("Bucket List", data.Buckets);
-//     }
-// });
-
 exports.putItem = function (AWS, fs, image, callback) {
     var _bucketName = 'nghiabc';
     var s3Bucket = new AWS.S3({params: {Bucket: _bucketName}});
@@ -50,25 +33,3 @@ exports.putItem = function (AWS, fs, image, callback) {
         }
     });
 };
-
-// // call S3 to retrieve upload file to specified bucket
-// var uploadParams = {Bucket: process.argv[2], Key: '', Body: ''};
-// var file = process.argv[3];
-// var fs = require('fs');
-// var fileStream = fs.createReadStream(file);
-// fileStream.on('error', function(err) {
-//     console.log('File Error', err);
-// });
-// uploadParams.Body = fileStream;
-//
-// var path = require('path');
-// uploadParams.Key = path.basename(file);
-//
-// // call S3 to retrieve upload file to specified bucket
-// s3.upload (uploadParams, function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } if (data) {
-//         console.log("Upload Success", data.Location);
-//     }
-// });
