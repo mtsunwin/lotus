605a21057a2f2391d64d744a02c38bb680eac48a
Tran Thang <mt.tranthang@gmail.com>
2017-12-17 23:25:05 +0700

    Update fill info for editprofile

--- public/js/controllers/editprofile.ctl.js
+++ public/js/controllers/editprofile.ctl.js
@@ -7,10 +7,22 @@ angular.module('myApp.controller.editprofile', [])
             birth: "",
             fb: "",
             gd: "",
-            address:""
+            address: "",
+            avatar: "../public/images/instagram/avatar.png"
         };
+        $scope.src = "../public/images/instagram/avatar.png";
+        Service.getOwnInfor(function (data) {
+            $scope.info.fullname = data.data.user.fullname;
+            $scope.src = typeof (data.data.user.avatar) != "undefined" ? data.data.user.avatar : "../public/images/instagram/avatar.png";
+            $scope.info.phone = data.data.user.phone;
+            $scope.info.birth = data.data.user.birthday;
+            $scope.info.address = typeof (data.data.user.address) != "undefined" ? data.data.user.address : "";
+            $scope.info.fb = typeof (data.data.user.accountFacebook) != "undefined" ? data.data.user.accountFacebook : "";
+            $scope.info.gd = typeof (data.data.user.accountGoogle) != "undefined" ? data.data.user.accountGoogle : "";
+            console.log(data);
+        })
         $scope.submit = function () {
-            Service.editProfile($scope.info.phone, $scope.info.birth, $scope.info.gd, $scope.info.fb, $scope.info.address,function (data) {
+            Service.editProfile($scope.info.phone, $scope.info.birth, $scope.info.gd, $scope.info.fb, $scope.info.address, function (data) {
                 console.log("oke");
             });
         }
